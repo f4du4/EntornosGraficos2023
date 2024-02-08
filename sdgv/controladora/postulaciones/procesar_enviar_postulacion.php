8ed98fbbfe480ecb69ae5eb7d8fde8bb79edb300
Fadua Dora <faduadora77@gmail.com>
2024-02-08 17:32:42 -0300

    Se agrego validacion a la hora de cargar archivos pdf para postularse

--- sdgv/controladora/postulaciones/procesar_enviar_postulacion.php
+++ sdgv/controladora/postulaciones/procesar_enviar_postulacion.php
@@ -1,44 +1,30 @@
 <?php
 include "../db/conexion.php";
 if (isset($_POST["submitenviar"])) {
-    if ($_FILES["pdfFile"]["error"] == UPLOAD_ERR_OK) {
-        // File information
-        $fileTmpPath = $_FILES["pdfFile"]["tmp_name"];
-        $pdfName = $_FILES["pdfFile"]["name"]; // Get the original name of the uploaded PDF
-
-        // Read the file content and convert it to binary
-        $pdfData = file_get_contents($fileTmpPath);
-
-        // Convert the binary PDF data to Base64 encoding
-        $base64Data = base64_encode($pdfData);
-        $idUsuario = $_POST["iduser"];
-        $idVacante = $_POST["idvac"];
-        $estado = "En espera";
-
-        $vQuery = "INSERT INTO postulaciones(estado, usuarios_id, vacantes_id, cv_nombre, cv_data) VALUES ('$estado', '$idUsuario', '$idVacante', '$pdfName', '$base64Data')";
-        mysqli_query($link, $vQuery) or die(mysqli_error($link));
-
-        $queryJefeCatedra = "SELECT usuarios.email, usuarios.nombre, usuarios.apellido, materias.nombreMat, vacantes.nombre as nombreVac FROM vacantes 
-        INNER JOIN materias ON vacantes.materia = materias.id INNER JOIN usuarios ON materias.jefecatedra_id = usuarios.id 
-        WHERE vacantes.id = $idVacante";
-        $resultJefeCatedra = mysqli_query($link, $queryJefeCatedra);
-        $rowJefeCatedra = mysqli_fetch_array($resultJefeCatedra);
-        $nombre = $rowJefeCatedra['nombre'];
-        $apellido = $rowJefeCatedra['apellido'];
-        $mate = $rowJefeCatedra['nombreMat'];
-        $vac = $rowJefeCatedra['nombreVac'];
-        $destino = $rowJefeCatedra['email'];
-        $asunto = "Notificacion Postulacion a Vacante";
-        $mensaje = "¡Hola {$nombre} {$apellido}! Este correo es para informarle que se ha postulado un usuario a su vacante de 
-                    la materia {$mate} para el puesto de {$vac}. Si desea visualizar la nueva postulacion, ingrese al sistema de 
-                    gestion de vacantes.";
-        $headers = "From:noreply@example.com\r\n";
-        $headers .= "Reply-To:noreply@example.com\r\n";
-        $headers .= "MIME-Version:1.0\r\n";
-        $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-
-        mail($destino, $asunto, $mensaje, $headers);
+    if (isset($_POST["iduser"], $_POST["idvac"], $_FILES["pdfFile"]) && !empty($_POST["iduser"]) && !empty($_POST["idvac"]) && !empty($_FILES["pdfFile"]["name"])) {
+        $extensionesPermitidas = ['pdf'];
+        $extensionDelArchivo = strtolower(pathinfo($_FILES["pdfFile"]["name"], PATHINFO_EXTENSION));
+        if (in_array($extensionDelArchivo, $extensionesPermitidas)) {
+            if ($_FILES["pdfFile"]["error"] == UPLOAD_ERR_OK) {
+                $pathTemporal = $_FILES["pdfFile"]["tmp_name"];
+                $nombrePdf = $_FILES["pdfFile"]["name"];
+                $dataPdf = file_get_contents($pathTemporal);
+                $pdfDataBase64 = base64_encode($dataPdf);
+                $idUsuario = mysqli_real_escape_string($link, $_POST["iduser"]);
+                $idVacante = mysqli_real_escape_string($link, $_POST["idvac"]);
+                $estado = "En espera";
+                $vQuery = "INSERT INTO postulaciones(estado, usuarios_id, vacantes_id, cv_nombre, cv_data) VALUES ('$estado', '$idUsuario', '$idVacante', '$nombrePdf', '$pdfDataBase64')";
+                mysqli_query($link, $vQuery) or die(mysqli_error($link));
+                header("Location: ../../paginas/postulaciones/cliente_postulaciones.php");
+                exit;
+            } else {
+                echo "Error uploading the file.";
+            }
+        } else {
+            echo "Only PDF files are allowed.";
+        }
+    } else {
+        echo "Please fill out all the required fields.";
     }
-    header("Location: ../../paginas/postulaciones/cliente_postulaciones.php");
 }
 mysqli_close($link);
